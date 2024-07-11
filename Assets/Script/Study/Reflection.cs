761636b75eee7f8fe372fbd974c183446f41622b
ajaykrishna1994 <36834069+ajaykrishna1994@users.noreply.github.com>
2024-07-11 14:58:29 +0530

    reflection

---
+++ Assets/Script/Study/Reflection.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class SampleClass
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Id: {Id}, Name: {Name}");
+    }
+}
+
+public class Reflection 
+{
+    // type is an instance of the Type class, and it holds metadata information about the SampleClass.
+   
+    void Start()
+    {
+        // It represents the metadata of a property in a class.
+        //Purpose: Represents an entire type (class, interface, enum, etc.).
+
+        Type type = typeof( SampleClass );
+        Console.WriteLine($"Type Name: {type.Name}");
+
+        // //Type and PropertyInfo store metadata, but they serve different
+        //Represents a single property of a type.
+        //Type of the property (e.g., int, string).
+        PropertyInfo[] properties = type.GetProperties();
+
+        foreach (PropertyInfo property in properties)
+        {
+            Console.WriteLine(property.Name);
+            Console.WriteLine(property.PropertyType.Name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
